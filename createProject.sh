0f2b3a89060be89695000c190a840325891ca765
江龙 <jianglong0156@sina.com>
2015-06-28 11:27:37 +0800

    add mac bat

---
+++ createProject.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+./src/build/gyp_chromium -Dgoogle_api_key='AIzaSyBN0lw6YmExMZpXnZ9WLGJjX5CYgPwMoTs' -Dgoogle_default_client_id='1077794207526-m59mciblhf5jlcnou33gki3prhu6r17p.apps.googleusercontent.com' -Dgoogle_default_client_secret='7f4d5ce93ed1afa7a8b83a4b817a0bfc7a0a60e0'
