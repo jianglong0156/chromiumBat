2de5510e8b8743d1aca3a15731b7245366abc3b6
jianglong0156 <jianglong0156@gmail.com>
2015-08-25 16:34:39 +0800

    update win32 sign bat

--- publish/sign/win/sign-exe.bat
+++ publish/sign/win/sign-exe.bat
@@ -1,8 +1,8 @@
 @echo off
 
-@set CMD_SIGNTOOL="C:\Program Files (x86)\Windows Kits\8.1\bin\x64\signtool.exe"
+@set CMD_SIGNTOOL="C:\Program Files (x86)\Windows Kits\10\bin\x64\signtool.exe"
 @set URL_TIMESTAMP="http://timestamp.comodoca.com/authenticode"
-@set CERT_PATH="%~dp0\installer-proj-win32\codesign\chukong.p12"
+@set CERT_PATH="chukong.p12"
 @set CERT_PASSWORD="chukong"
 
-@%CMD_SIGNTOOL% sign /v /f %CERT_PATH% /p %CERT_PASSWORD% /t %URL_TIMESTAMP% "%~dp0\..\release\CocosFramework.exe"
+@%CMD_SIGNTOOL% sign /v /f %CERT_PATH% /p %CERT_PASSWORD% /t %URL_TIMESTAMP% "%~dp0\..\..\CocosSimulatorWin_v1.0.exe"
